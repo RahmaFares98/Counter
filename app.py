7905445cb83ccdc9f9665e2e0afe63f696ed81f5
rahmafaris981 <rahmafaris981@gmail.com>
2024-06-24 16:39:16 +0300

    Counter

--- app.py
+++ app.py
@@ -7,37 +7,36 @@ app.secret_key = 'secret_key'  # for session
 def index():
     if 'visit_count' not in session:
         session['visit_count'] = 0
+    if session.get('flag', True):
+        session['visit_count'] += 1  # Increment visit_count on every visit
+    session['flag'] = True  # Reset the flag after checking
     if 'counter' not in session:
         session['counter'] = 0
-    session['visit_count'] += 1  # Increment visit_count on every visit
     return render_template('index.html', visit_count=session['visit_count'], counter=session['counter'])
 
 @app.route('/incrementby2', methods=['POST'])
 def increment_by_2():
     session['counter'] += 2
-    session['visit_count'] += 2
-    return redirect(url_for('index'))  # action /incrementby2
+    session['flag'] = False  # Prevent visit_count increment on redirect
+    return redirect(url_for('index'))
 
 @app.route('/increment', methods=['POST'])
 def increment():
     increment_value = int(request.form.get('increment_value', 1))
     session['counter'] += increment_value
-    session['visit_count'] += increment_value
-    return redirect(url_for('index'))  # action /increment
+    session['flag'] = False  # Prevent visit_count increment on redirect
+    return redirect(url_for('index'))
 
 @app.route('/destroy_session', methods=['GET'])
 def destroy_session():
     session.clear()
-    return redirect(url_for('index'))  # action destroy_session
+    return redirect(url_for('index'))
 
 @app.route('/reset', methods=['GET'])
 def reset():
     session['counter'] = 0
-    return redirect(url_for('index'))  # action /reset
+    session['flag'] = False  # Prevent visit_count increment on redirect
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-# # python 
-# import base64   
-# base64.urlsafe_b64decode('eyJjb3VudGVyIjoyLCJ2aXNpdF9jb3VudCI6M30===')
